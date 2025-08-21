efe13925e2d1589d61df86a00bdcfa2b327fb4a1
JamMack <55844374+JamMack@users.noreply.github.com>
2025-08-21 10:39:23 +0000

    Deploying to gh-pages from @ JamMack/jammack.github.io@7e1070710721b144aff4660a8cd800a73d351877 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,7 +45,7 @@ ninja.data = [{
             window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
-          title: "Teaching",
+          title: "teaching",
           description: "",
           section: "Navigation",
           handler: () => {
